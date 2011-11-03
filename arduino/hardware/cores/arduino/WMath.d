901e4b83b8672da0eca759eadb041624d598311a
Tamas Kemenczy <tkemenczy@gmail.com>
2011-11-02 21:45:48 -0500

    Added modified arduino library source, updated README

---
+++ arduino/hardware/cores/arduino/WMath.d
@@ -0,0 +1,3 @@
+/home/tamas/opt/arduino-0016/hardware/cores/arduino/WMath.o /home/tamas/opt/arduino-0016/hardware/cores/arduino/WMath.d: /home/tamas/opt/arduino-0016/hardware/cores/arduino/WMath.cpp \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/stdlib.h \
+  /usr/lib/gcc/avr/4.3.2/include/stddef.h
---
+++ arduino/hardware/cores/arduino/wiring_analog.d
@@ -0,0 +1,23 @@
+/home/tamas/opt/arduino-0016/hardware/cores/arduino/wiring_analog.o /home/tamas/opt/arduino-0016/hardware/cores/arduino/wiring_analog.d:  \
+ /home/tamas/opt/arduino-0016/hardware/cores/arduino/wiring_analog.c \
+  /home/tamas/opt/arduino-0016/hardware/cores/arduino/wiring_private.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/io.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/sfr_defs.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/inttypes.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/stdint.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/iom328p.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/portpins.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/common.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/version.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/fuse.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/lock.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/interrupt.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/util/delay.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/util/delay_basic.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/stdio.h \
+  /usr/lib/gcc/avr/4.3.2/include/stdarg.h \
+  /usr/lib/gcc/avr/4.3.2/include/stddef.h \
+  /home/tamas/opt/arduino-0016/hardware/cores/arduino/wiring.h \
+  /home/tamas/opt/arduino-0016/hardware/cores/arduino/binary.h \
+  /home/tamas/opt/arduino-0016/hardware/cores/arduino/pins_arduino.h \
+  /usr/lib/gcc/avr/4.3.2/../../../avr/include/avr/pgmspace.h
